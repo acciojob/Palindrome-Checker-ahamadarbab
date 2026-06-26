87bab046af6c84c404eb2657e37c25b2633db2c8
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-06-26 17:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
+	const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+	const revered = cleaned.split("").reverse().join("");
+
+	return revered === cleaned;
 }
 module.exports = palindrome
